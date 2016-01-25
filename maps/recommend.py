aca08eb374430c2d948baa87bce7755934d52ec2
tavaresdong <tavares.nju@gmail.com>
2016-01-24 18:39:37 -0800

    question 3

--- maps/recommend.py
+++ maps/recommend.py
@@ -19,7 +19,7 @@ def find_closest(location, centroids):
     [2.0, 3.0]
     """
     # BEGIN Question 3
-    "*** REPLACE THIS LINE ***"
+    return min(centroids, key=lambda c : distance(c, location))
     # END Question 3
 
 
