75e53ae971ce1c2cd71291a5dac355869a682c5a
tavaresdong <tavares.nju@gmail.com>
2016-03-15 18:22:57 -0700

    q2 q3 done

---
+++ lab/lab09/lab09.scm
@@ -0,0 +1,58 @@
+;; Scheme ;;
+
+; Q2
+(define (cube x)
+  (* x x x)
+)
+
+
+; Q3
+(define (over-or-under x y)
+  (if (< x y)
+      -1
+      (if (= x y)
+          0
+          1
+      )
+  )
+)
+
+
+; Q4
+(define lst
+  'YOUR-CODE-HERE
+)
+  
+
+; Q5
+(define (remove item lst)
+  'YOUR-CODE-HERE
+)
+
+;;; Tests
+
+(remove 3 nil)
+; expect ()
+(remove 3 '(1 3 5))
+; expect (1 5)
+(remove 5 '(5 3 5 5 1 4 5 4))
+; expect (3 1 4 4)
+
+
+; Q6
+(define (filter f lst)
+  'YOUR-CODE-HERE
+)
+
+
+; Q7
+(define (make-adder num)
+  'YOUR-CODE-HERE
+)
+
+
+; Q8
+(define (composed f g)
+  'YOUR-CODE-HERE
+)
+
