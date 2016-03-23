b282c8c7dc73a1789708614611e90ff954feea30
tavaresdong <tavares.nju@gmail.com>
2016-03-22 18:10:45 -0700

    problem 1

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -169,6 +169,7 @@ class HarvesterAnt(Ant):
 
     name = 'Harvester'
     implemented = True
+    food_cost = 2
 
     def action(self, colony):
         """Produce 1 additional food for the colony.
@@ -176,7 +177,7 @@ class HarvesterAnt(Ant):
         colony -- The AntColony, used to access game state information.
         """
         # BEGIN Problem 1
-        "*** REPLACE THIS LINE ***"
+        colony.food += 1
         # END Problem 1
 
 
@@ -186,6 +187,7 @@ class ThrowerAnt(Ant):
     name = 'Thrower'
     implemented = True
     damage = 1
+    food_cost = 4
 
     def nearest_bee(self, hive):
         """Return the nearest Bee in a Place that is not the Hive, connected to
